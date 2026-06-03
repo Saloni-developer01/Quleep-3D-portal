3b0bef83423d06b9bb1137bc855f78f6be8a51bb
unknown <webhostingportfolio124@gmail.com>
2026-06-03 16:36:47 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://quleep-3d-portal.onrender.com/api/products'
+  baseURL: 'https://quleep-3d-portal.onrender.com/api'
 });
 
 export const getProducts = (search = '') => API.get(`?search=${search}`);
